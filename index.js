5199822d25fa06870013c956a779a0f74f6d6233
Steve Kinney <hello@stevekinney.net>
2015-04-29 10:49:10 -0600

    Set up some basic chats

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ app.get('/', function (req, res){
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+io.on('connection', function (socket) {
+  console.log('SOMEONE CONNECTED!!! THIS IS GREAT!!!');
+  socket.emit('message', { user: 'turingbot', text: 'Hello and welcome to this chatroom.' });
+  socket.broadcast.emit('message', { user: 'turingbot', text: 'Someone new has connected.' });
+
+  socket.on('message', function (channel, message) {
+    io.sockets.emit('message', message);
+  });
+});
+
 http.listen(3000, function(){
   console.log('Your server is up and running on Port 3000. Good job!');
 });
