fb40b041e49feda155ee3259afcfc3f2afe8f555
Steve Kinney <hello@stevekinney.net>
2015-06-11 09:29:52 -0600

    Remove some stuff

--- index.js
+++ index.js
@@ -11,16 +11,6 @@ app.get('/', function (req, res){
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-io.on('connection', function (socket) {
-  console.log('SOMEONE CONNECTED!!! THIS IS GREAT!!!');
-  socket.emit('message', { user: 'turingbot', text: 'Hello and welcome to this chatroom.' });
-  socket.broadcast.emit('message', { user: 'turingbot', text: 'Someone new has connected.' });
-
-  socket.on('message', function (channel, message) {
-    io.sockets.emit('message', message);
-  });
-});
-
 http.listen(process.env.PORT, function(){
   console.log('Your server is up and running on Port 3000. Good job!');
 });
